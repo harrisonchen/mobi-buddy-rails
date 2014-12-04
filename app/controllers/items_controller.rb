530238fed2c1a786409ee9308d047faaf009854c
Harrison Chen <harrisonxchen@gmail.com>
2014-12-03 16:54:01 -0800

    lala

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,9 +10,9 @@ class ItemsController < ApplicationController
 											 price: params[:price],
 											 category: params[:category],
 											 image_url: params[:image_url],
+											 store_name: params[:store_name]),
 											 lat: params[:lat],
-											 long: params[:long],
-											 store_name: params[:store_name])
+											 long: params[:long]
 
 		render :json => { success: true, item: item.as_json }
 	end
